68fc8bee73b39f6984f39506f63ec20214e56ef9
Ken Wills <ken@zaptillion.net>
2018-03-12 17:19:31 -0500

    update sso test script, add alias for infispan modules cachestore.remote -> persistence.remote

--- misc/sso-oc-cluster.sh
+++ misc/sso-oc-cluster.sh
@@ -13,4 +13,7 @@ docker tag jboss-eap-7-tech-preview/eap-online-openshift:12.0 $CLUSTER_IP:5000/o
 docker tag jboss-eap-7-tech-preview/eap-online-openshift:12.0 $CLUSTER_IP:5000/openshift/eap-online-openshift:12.0
 docker push $CLUSTER_IP:5000/openshift/eap-online-openshift:12
 docker push $CLUSTER_IP:5000/openshift/eap-online-openshift:12.0
+oc create -n myproject -f secrets
 oc process -n myproject -f templates/eap-online/eap-online-sso-s2i.json -p APPLICATION_NAME=helloworld-sso -p HOSTNAME_HTTP=localhost -p HOSTNAME_HTTPS=localhost -pSSO_URL=http://localhost/ -pSSO_REALM=helloworld-sso | oc create -n myproject -f -
+
+
