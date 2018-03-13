dfc6421f59c06d4e7594495f9d7e8b275d366c7b
Ken Wills <ken@zaptillion.net>
2018-03-12 19:03:14 -0500

    remove dup shell test script lines

---
+++ misc/setup-templates.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+oc cluster up # start cluster
+oc login -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin developer
+oc login -u developer
+oc create route edge --service=docker-registry -n default
+oc create -n openshift -f templates/eap-online-image-streams.json
+oc create -n myproject -f secrets
+sleep 5
+AUTH=`oc whoami -t`
+CLUSTER_IP=`oc get -n default svc/docker-registry -o=yaml | grep clusterIP  | awk -F: '{print $2}'`
+docker login -u developer -p $AUTH $CLUSTER_IP:5000
+docker tag jboss-eap-7-tech-preview/eap-online-openshift:12.0 $CLUSTER_IP:5000/openshift/eap-online-openshift:12
+docker tag jboss-eap-7-tech-preview/eap-online-openshift:12.0 $CLUSTER_IP:5000/openshift/eap-online-openshift:12.0
+docker push $CLUSTER_IP:5000/openshift/eap-online-openshift:12
+docker push $CLUSTER_IP:5000/openshift/eap-online-openshift:12.0
