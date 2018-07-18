16de2f75a1ec639ea2f33e935162845934393568
Marek Goldmann <mgoldman@redhat.com>
2018-07-18 10:56:57 +0200

    Change properties used to define image streams

--- tests/arq/src/test/java/org/jboss/test/arquillian/ce/common/EapDbTestBase.java
+++ tests/arq/src/test/java/org/jboss/test/arquillian/ce/common/EapDbTestBase.java
@@ -13,7 +13,7 @@ import org.junit.Test;
  * @author Marko Luksa
  */
 @OpenShiftResource("${openshift.imageStreams}")
-@OpenShiftDynamicImageStreamResource(name = "${imageStream.eap71.name:jboss-eap71-openshift}", image = "${imageStream.eap71.image:registry.access.redhat.com/jboss-eap-7/eap71-openshift:1.3}", version = "${imageStream.eap71.version:1.3}")
+@OpenShiftDynamicImageStreamResource(name = "${image.stream.name}", image = "${image.stream.image}", version = "${image.stream.version}")
 public abstract class EapDbTestBase {
     @RouteURL("eap-app")
     private URL url;
